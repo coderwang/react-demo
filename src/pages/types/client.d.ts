449448a7fc83636a53c4b898576458064c66ace7
wangshuhan <wangshuhan@kanzhun.com>
2024-09-26 17:34:45 +0800

    feat: install husky

--- src/pages/types/client.d.ts
+++ src/pages/types/client.d.ts
@@ -31,9 +31,7 @@ declare module '*.webp' {
 declare module '*.svg' {
 	import type * as React from 'react';
 
-	export const ReactComponent: React.FunctionComponent<
-		React.SVGProps<SVGSVGElement>
-	>;
+	export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 
 	const src: string;
 	export default src;
