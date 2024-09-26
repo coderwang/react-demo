b0f8c5a4242cb4ab5e5f50ce8bdc12f11cf940b3
wangshuhan <wangshuhan@kanzhun.com>
2024-09-26 16:01:54 +0800

    feat: 支持px转vw

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const pxToViewport = require('postcss-px-to-viewport');
 
 module.exports = {
 	entry: './src/index.tsx',
@@ -20,7 +21,30 @@ module.exports = {
 			},
 			{
 				test: /\.less$/,
-				use: ['style-loader', 'css-loader', 'less-loader'],
+				use: [
+					'style-loader',
+					'css-loader',
+					'less-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							postcssOptions: {
+								plugins: [
+									pxToViewport({
+										viewportWidth: 375, // 设计稿的视口宽度
+										viewportUnit: 'vw', // 希望使用的视口单位
+										fontViewportUnit: 'vw', // 字体使用的视口单位
+										selectorBlackList: ['.ignore'], // 需要忽略的CSS选择器
+										minPixelValue: 1, // 最小的转换数值
+										mediaQuery: false, // 是否在媒体查询的css代码中也进行转换
+										replace: true, // 是否直接更换属性值
+										exclude: [/node_modules/], // 忽略某些文件夹下的文件或特定文件
+									}),
+								],
+							},
+						},
+					},
+				],
 			},
 			{
 				test: /\.(png|jpg|jpeg|gif)$/,
