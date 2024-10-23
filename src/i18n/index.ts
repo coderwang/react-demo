1ca541b716a8237173bb4381d5c0fe22cae55eaa
wangshuhan <wangshuhan@kanzhun.com>
2024-10-23 15:51:17 +0800

    chore: i18n-ally配置

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,14 +1,14 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enCommon from './en/common.json';
-import zhCommon from './zh/common.json';
+import enResource from './locales/en.json';
+import zhResource from './locales/zh.json';
 
 const resources = {
 	en: {
-		common: enCommon,
+		translation: enResource,
 	},
 	zh: {
-		common: zhCommon,
+		translation: zhResource,
 	},
 };
 
@@ -16,7 +16,7 @@ i18n.use(initReactI18next).init({
 	resources,
 	lng: navigator.language.substring(0, 2) === 'zh' ? 'zh' : 'en', // 默认语言
 	fallbackLng: 'en', // 兜底语言
-	defaultNS: 'common', // 默认命名空间
+	defaultNS: 'translation', // 默认命名空间
 	interpolation: {
 		escapeValue: false, // 无需转移，react已是xss安全
 	},
